1638b9a7a4bfde26d17def704fd92463113e866b
khangksp <129304975+khangksp@users.noreply.github.com>
2024-12-04 12:17:55 +0700

    second commit
    yo

--- odms/includes/dbconnection.php
+++ odms/includes/dbconnection.php
@@ -1,16 +1,19 @@
 <?php 
-// DB credentials.
-define('DB_HOST','mysql');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','odmsdb');
+// DB credentials từ biến môi trường
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
+define('DB_NAME', getenv('DB_NAME'));
+
 // Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
+try {
+    $dbh = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
+        DB_USER,
+        DB_PASS,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
+    );
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
 }
 ?>
